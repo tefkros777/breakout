89bea252e24442415339a0ca408fa5a4f10b813f
tefkros <constantinoslo98@gmail.com>
2021-03-10 23:38:49 +0000

    Add PlayerInput component for Launchpad

---
+++ Assets/Scripts/Behaviours/LaunchpadController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LaunchpadController : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.5f;
+
+    private Rigidbody2D rigidbody;
+    
+    private Vector2 mInputDirection;
+
+    private void Awake()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move(mInputDirection);
+    }
+
+    public void OnMove(InputAction.CallbackContext input)
+    {
+        mInputDirection = input.ReadValue<Vector2>();
+    }
+
+    public void Move(Vector2 direction)
+    {
+        var myVelocity = rigidbody.velocity;
+        myVelocity.x = direction.x * speed;
+        rigidbody.velocity = myVelocity;
+    }
+
+
+}
