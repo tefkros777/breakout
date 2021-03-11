4cf4dcfbcdf31a27674b26611fdc98fb5a329312
tefkros <constantinoslo98@gmail.com>
2021-03-11 14:25:00 +0000

    Add DockCheck method in BallController

--- Assets/Scripts/Behaviours/BallController.cs
+++ Assets/Scripts/Behaviours/BallController.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rigidbody;
+    private Collider2D ballCollider;
+    private Collider2D dockCollider;
+
+    private bool isDocked = false;
+
+    private void Awake()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        ballCollider = gameObject.GetComponent<CircleCollider2D>();
+        dockCollider = GameObject.FindGameObjectWithTag("Dock").GetComponent<CapsuleCollider2D>();
+        isDocked = false;
+    }
+
     void Update()
     {
-        
+        dockCheck();
+    }
+
+    private void dockCheck()
+    {
+        if (ballCollider.IsTouching(dockCollider)){
+            isDocked = true;
+        }
+        else
+        {
+            isDocked = false;
+        }
     }
 }
