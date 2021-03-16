ca866ec8d1ca435983d8329a06c6626d81c6a922
tefkros <constantinoslo98@gmail.com>
2021-03-16 14:04:10 +0000

    Blocks take damage on collision

--- Assets/Scripts/Behaviours/BlockController.cs
+++ Assets/Scripts/Behaviours/BlockController.cs
@@ -1,15 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BlockController : MonoBehaviour
 {
+    private int health;
+
+    private void Awake()
+    {
+        health = 3;
+    }
+
     void Update()
     {
         
     }
 
     // Code for destoying
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        takeDamage();
+    }
+
+    private void takeDamage()
+    {
+        health--;
+
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+
     // Number of lives
 
 }
