0df5761f9f1cbe5284ef07ee38a504d00425998c
tefkros <constantinoslo98@gmail.com>
2021-03-18 16:24:32 +0000

    Add code for changing block sprite to cracked sprite

--- Assets/Scripts/Behaviours/BlockController.cs
+++ Assets/Scripts/Behaviours/BlockController.cs
@@ -3,23 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class BlockController : MonoBehaviour
 {
     private static readonly int DEFAULT_HEALTH;
+    private static readonly int HEALTHY_SPRITE_INDEX = 0;
+    private static readonly int CRACKED_SPRITE_INDEX = 1;
 
     // Number of hits before destroying
     [SerializeField] private int health = DEFAULT_HEALTH;
+    [SerializeField] private Sprite[] sprites;
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        takeDamage();
+        TakeDamage();
+        if (health <= health / 2)
+            SetCrackedSprite();
     }
 
     // Code for destoying
-    private void takeDamage()
+    private void TakeDamage()
     {
         health--;
         if (health <= 0)
             Destroy(gameObject);
+    }
+
+    private void SetCrackedSprite()
+    {
+        Debug.Log("Set cracked sprite");
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[CRACKED_SPRITE_INDEX];
     }
 }
