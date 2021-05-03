63ebba28831635d8f71f1ab60be90f9003611d9c
tefkros <constantinoslo98@gmail.com>
2021-05-03 18:00:46 +0100

    Menu buttons work

--- Assets/Scripts/Behaviours/MainMenuManager.cs
+++ Assets/Scripts/Behaviours/MainMenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -8,11 +9,25 @@ public class MainMenuManager : MonoBehaviour
     public void LaunchGame()
     {
         Debug.Log("Launching Level");
+        StartCoroutine(LoadLevel1Async());
     }
 
     public void ExitGame()
     {
         Debug.Log("Exit Game");
+        Application.Quit();
+    }
+
+    // Co-routine - Keep checking for if the scene is loaded
+    IEnumerator LoadLevel1Async()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Level1");
+        
+        // If loading is not finished
+        while (!asyncLoad.isDone)
+        {
+            yield return null; // Not yet done
+        }
     }
 
 }
