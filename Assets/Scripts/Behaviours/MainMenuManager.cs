6e38534d73bd0572df0e7e0fa57ae9a5636698c8
tefkros <constantinoslo98@gmail.com>
2021-05-03 17:46:29 +0100

    Add main menu scene

---
+++ Assets/Scripts/Behaviours/MainMenuManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    
+    public void LaunchGame()
+    {
+        Debug.Log("Launching Level");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit Game");
+    }
+
+}
