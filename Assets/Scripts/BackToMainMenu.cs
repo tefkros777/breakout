b82cd70ecae5662875de9ccd4a6f74bd3e17af02
tefkros <constantinoslo98@gmail.com>
2021-05-05 14:34:19 +0100

    Leaderboard works with animation

--- Assets/Scripts/BackToMainMenu.cs
+++ Assets/Scripts/BackToMainMenu.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BackToMainMenu : MonoBehaviour
 {
+    public Animator Anim;
+    public Image img;
+
     public void LoadMainMenu()
     {
         Debug.Log("GOING BACK TO MENU");
@@ -13,6 +17,9 @@ public class BackToMainMenu : MonoBehaviour
 
     IEnumerator GoToMainMenu()
     {
+        Anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => img.color.a == 1);
+
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainMenu");
 
         // If loading is not finished
